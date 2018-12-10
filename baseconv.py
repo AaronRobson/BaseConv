62c602b2f56b39cd7baf79f13b00c701caa81b54
Aaron Robson <AaronRobson@users.noreply.github.com>
2018-12-10 22:42:40 +0000

    pep8 checks and fixes

--- baseconv.py
+++ baseconv.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-from string import digits, ascii_uppercase #, ascii_lowercase
+from string import digits, ascii_uppercase
 
 from six.moves import input
 
@@ -10,53 +10,74 @@ ZERO = '0'
 DEFAULT_BASE = 10
 MINIMUM_BASE = 2
 
-#the index of a character in this collection should give the value (as long as the character is uppercased in standard notation)
-ALLOWED_SYMBOLS = digits + ascii_uppercase # + ascii_lowercase
+# the index of a character in this collection should give the value
+# (as long as the character is uppercased in standard notation)
+ALLOWED_SYMBOLS = digits + ascii_uppercase
 
-#http://en.wikipedia.org/wiki/Radix
-class BaseConvError(Exception): #generic attributes for all custom exceptions
+# http://en.wikipedia.org/wiki/Radix
+
+
+class BaseConvError(Exception):
+    '''Generic attributes for all custom exceptions.
+    '''
+
     def __str__(self):
-        return str(self._value) # __repr__ defaults to the value of __str__ when missing
+        '''__repr__ defaults to the value of __str__ when missing.
+        '''
+        return str(self._value)
+
 
 class InvalidNumberError(BaseConvError):
     _value = 'Invalid Number'
 
+
 class InvalidBaseError(BaseConvError):
     _value = 'Invalid Base'
+
 
 class InvalidInputBaseError(InvalidBaseError):
     _value = 'Invalid Input Base'
 
+
 class InvalidInternalValueError(BaseConvError):
     _value = 'Invalid Internal Value Error'
 
+
 def _ValidNum(value):
-    '''Returns a string representing the number given validated to standard notation:
-    no leading zeros (with or without a minus sign) and all letters in uppercase format.
+    '''Return a string representing the number given validated to
+    standard notation:
+    no leading zeros (with optional minus sign)
+    all letters in uppercase format.
 
-    Throws InvalidNumberError on data with disallowed symbols.'''
+    Throws InvalidNumberError on data with disallowed symbols.
+    '''
 
-    # does not presume to assume if the number is int that it must be decimal as it could also represent any other lower base
+    # does not presume to assume if the number is int that it must be decimal
+    # as it could also represent any other lower base
     value = str(value).strip()
-    # would if not alpha try converting to int and letting conversion errors not be caught and go straight to the call (or be replaced by a custom InvalidBaseError exception), however this way just removes all non-allowed characters instead
+    # would if not alpha try converting to int and letting conversion errors
+    # not be caught and go straight to the call (or be replaced by a custom
+    # InvalidBaseError exception), however this way just removes all
+    # non-allowed characters instead
     value = value.upper()
 
     minused = value.startswith(MINUS_SIGN)
-    #remove minus sign if present
+    # remove minus sign if present
     if minused:
         value = value[1:]
 
-    #remove any leading zeros
+    # remove any leading zeros
     value = value.lstrip(ZERO)
 
     if value:
         for char in value:
-            if not char in ALLOWED_SYMBOLS:
+            if char not in ALLOWED_SYMBOLS:
                 raise InvalidNumberError
         else:
-            return (MINUS_SIGN * minused) + value        
+            return (MINUS_SIGN * minused) + value
     else:
         return ZERO
+
 
 def _ValidBas(value):
     '''Returns an int with a value of 2 or above or
@@ -74,9 +95,11 @@ def _ValidBas(value):
         raise InvalidBaseError
     else:
         if MINIMUM_BASE <= intValue:
-            return intValue # nominal case
+            # nominal case
+            return intValue
         else:
             raise InvalidBaseError
+
 
 def _ValToChar(inputNum):
     try:
@@ -93,6 +116,7 @@ def _ValToChar(inputNum):
     except IndexError:
         raise InvalidInternalValueError
 
+
 def IntoDec(inNum, inBas):
     '''Returns an int.
     '''
@@ -102,6 +126,7 @@ def IntoDec(inNum, inBas):
         return int(inNum, inBas)
     except ValueError:
         raise InvalidInputBaseError
+
 
 def FromDec(inNum, outBas):
     '''Is an error for inNum to not be an integer.
@@ -119,28 +144,35 @@ def FromDec(inNum, outBas):
 
     values = []
     while (0 < inNum):
-        #is quicker to put in a list and then join at the end
         values.append(_ValToChar(inNum % outBas))
         inNum //= outBas
 
     value = ''.join(reversed(values))
- 
+
     if value:
-        return (MINUS_SIGN * minused) + value 
+        return (MINUS_SIGN * minused) + value
     else:
         return ZERO
 
+
 def BasCalc(inNum, inBas=DEFAULT_BASE, outBas=DEFAULT_BASE):
-    '''Given a number and its current base, returns the number with a new specified base.
+    '''Given a number and its current base, returns the number with a
+    new specified base.
     If a base is not given it is assumed to be base %d.
     ''' % (DEFAULT_BASE)
     return FromDec(IntoDec(inNum, inBas), outBas)
+
 
 def _CommandLine(args):
     if 1 < len(args):
         for arg in args[1:]:
             if arg in ('-h', '--help'):
-                print('This program converts integers which may be signed between any two number bases %d and over.\nInputs as follows:\ninNum = the Input Number\ninBas = the Input Base\noutBas = the Output Base' % (MINIMUM_BASE))
+                print('This program converts integers which may be signed ' +
+                      'between any two number bases %d and over.\n' +
+                      'Inputs as follows:\n' +
+                      'inNum = the Input Number\n' +
+                      'inBas = the Input Base\n' +
+                      'outBas = the Output Base' % (MINIMUM_BASE))
                 break
             elif arg in ('-t', '--test'):
                 import test
@@ -153,15 +185,18 @@ def _CommandLine(args):
         exitVals = ('q', 'quit')
         while True:
             try:
-                print('Output Number: ' + BasCalc(input('\nEnter an Input Number: ').strip(), input('Enter an Input Base: ').strip(), input('Enter an Output Base: ').strip()))
+                print('Output Number: ' +
+                      BasCalc(input('\nEnter an Input Number: ').strip(),
+                              input('Enter an Input Base: ').strip(),
+                              input('Enter an Output Base: ').strip()))
             except (BaseConvError, ValueError) as e:
                 print('Error: ', e)
-            if input('\nEnter any of the following values to exit: %s\nor press return to continue: ' % (str(exitVals))).strip().lower() in exitVals:
+            if input('\nEnter any of the following values to exit: %s\n' +
+                     'or press return to continue: ' %
+                     (str(exitVals))).strip().lower() in exitVals:
                 break
+
 
 if __name__ == "__main__":
     from sys import argv
     _CommandLine(argv)
-
-    #keep the window open
-    #input('\nPress Enter to Exit:')
--- tests/test_library.py
+++ tests/test_library.py
@@ -2,38 +2,44 @@
 
 import unittest
 
-import baseconv
+from baseconv import IntoDec, FromDec, BasCalc
+
 
 class TestBaseConvClass(unittest.TestCase):
-    def setUp(self):
-        self.support = baseconv
 
     def testIntoDec(self):
-        self.assertEqual(self.support.IntoDec(10, 2), 2, 'IntoDec Fail: from binary simple.')
-        self.assertEqual(self.support.IntoDec(-10, 2), -2, 'IntoDec Fail: from binary simple but negative.')
-        self.assertEqual(self.support.IntoDec(101010, 2), 42, 'IntoDec Fail: from binary complex 42.')
-        self.assertEqual(self.support.IntoDec('9', 10), 9, 'IntoDec fail: under boundary condition')
+        self.assertEqual(IntoDec(10, 2), 2, 'from binary')
+        self.assertEqual(IntoDec(-10, 2), -2, 'from binary negative')
+        self.assertEqual(IntoDec(101010, 2), 42, 'from binary 42')
+        self.assertEqual(IntoDec('9', 10), 9, 'under boundary condition')
 
     def testFromDec(self):
-        self.assertEqual(self.support.FromDec(15, 16), 'F', 'FromDec Fail: edge.')
-        self.assertEqual(self.support.FromDec(-15, 16), '-F', 'FromDec Fail: edge minus.')
-        self.assertEqual(self.support.FromDec(42, 2), '101010', 'FromDec Fail: to boolean')
-      
-    def testBasCalc(self):
-        self.assertEqual(self.support.BasCalc(101010, 2, 10), '42', 'BasCalc Fail: binary to decimal.')
-        self.assertEqual(self.support.BasCalc('101010', '2', '10'), '42', 'BasCalc Fail: text binary to decimal.')
-        self.assertEqual(self.support.BasCalc(42, 10, 2), '101010', 'BasCalc Fail: decimal to binary.')
-        self.assertEqual(self.support.BasCalc('101010', inBas=2), '42', 'BasCalc fail: default out base with named in base.')
-        self.assertEqual(self.support.BasCalc(42, outBas=2), '101010', 'BasCalc Fail: default in base with named out base.')
-        self.assertEqual(self.support.BasCalc(15, inBas=6, outBas=4), '23', 'BasCalc fail: non standard named in and out bases')
-        self.assertEqual(self.support.BasCalc('FF', 16, 2), '11111111', 'BasCalc Fail: large hex to binary.')
-        self.assertEqual(self.support.BasCalc(11111111, 2, 16), 'FF', 'BasCalc Fail: large binary to hex.')
-        self.assertEqual(self.support.BasCalc('400', 16, 10), '1024', 'BasCalc Fail: large hex to decimal.')
+        self.assertEqual(FromDec(15, 16), 'F', 'edge')
+        self.assertEqual(FromDec(-15, 16), '-F', 'edge minus')
+        self.assertEqual(FromDec(42, 2), '101010', 'to binary')
 
-        self.assertEqual(self.support.BasCalc('-567', 10, 10), '-567', 'BasCalc Fail: minus number incorrectly handled in simple decimal to decimal case.')
+    def testBasCalc(self):
+        self.assertEqual(BasCalc(101010, 2, 10), '42', 'binary to decimal')
+        self.assertEqual(BasCalc('101010', '2', '10'), '42',
+                         'text binary to decimal')
+        self.assertEqual(BasCalc(42, 10, 2), '101010', 'decimal to binary')
+        self.assertEqual(BasCalc('101010', inBas=2), '42',
+                         'default out base, named in base')
+        self.assertEqual(BasCalc(42, outBas=2), '101010',
+                         'default in base, named out base')
+        self.assertEqual(BasCalc(15, inBas=6, outBas=4), '23',
+                         'named in and out bases')
+        self.assertEqual(BasCalc('FF', 16, 2), '11111111', 'hex to binary')
+        self.assertEqual(BasCalc(11111111, 2, 16), 'FF', 'binary to hex')
+        self.assertEqual(BasCalc('400', 16, 10), '1024', 'hex to decimal')
+
+        self.assertEqual(BasCalc('-567', 10, 10), '-567',
+                         'minus number decimal to decimal')
 
         for i in range(1000):
-          self.assertEqual(self.support.BasCalc('DEADBEEF', 16, 2), '11011110101011011011111011101111', '')
+            self.assertEqual(BasCalc('DEADBEEF', 16, 2),
+                             '11011110101011011011111011101111')
+
 
 if __name__ == "__main__":
     unittest.main()
