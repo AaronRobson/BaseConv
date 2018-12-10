14fa79d975f4d77bef52a8309632483606e170d9
Aaron Robson <AaronRobson@users.noreply.github.com>
2018-12-10 22:01:28 +0000

    Testing GUI (more than just importing it)

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -1,6 +1,17 @@
 import unittest
+import string
 
-import baseconvgui
+from baseconvgui import OnValidateGiven
+
+
+class OnValidateGivenTest(unittest.TestCase):
+
+    def test_valid(self):
+        self.assertTrue(OnValidateGiven(string.digits[0], string.digits))
+
+    def test_invalid(self):
+        self.assertFalse(OnValidateGiven('*', string.digits))
+
 
 if __name__ == "__main__":
     unittest.main()
